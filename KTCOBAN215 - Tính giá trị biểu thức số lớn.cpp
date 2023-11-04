1f03f2b9555a993231b216a5609d9f26c6998754
Đoàn Văn Điệp <147433897+doanvandiep@users.noreply.github.com>
2023-11-04 20:34:59 +0700

    KTCOBAN215 - Tính giá trị biểu thức số lớn.cpp

--- KTCOBAN215 - Tính giá trị biểu thức số lớn.cpp
+++ KTCOBAN215 - Tính giá trị biểu thức số lớn.cpp
@@ -1,52 +1,50 @@
-#include <iostream>
-#include <string>
+#include<bits/stdc++.h>
 using namespace std;
-
-string multiply(string num1, string num2) {
-    int len1 = num1.size();
-    int len2 = num2.size();
-    string result(len1 + len2, '0');
-
-    for (int i = len1 - 1; i >= 0; i--) {
-        int carry = 0;
-        for (int j = len2 - 1; j >= 0; j--) {
-            int temp = (num1[i] - '0') * (num2[j] - '0') + (result[i + j + 1] - '0') + carry;
-            carry = temp / 10;
-            result[i + j + 1] = (temp % 10) + '0';
-        }
-        result[i] += carry;
-    }
-    size_t pos = result.find_first_not_of('0');
-    if (pos != string::npos) {
-        return result.substr(pos);
-    }
-
-    return "0";
-}
-
-string calculateProduct(int a, int b) {
-    string product = "1";
-    for (int i = a; i <= b; i++) {
-        string multiplier = to_string(i);
-        product = multiply(product, multiplier);
-    }
-    return product;
-}
-
-int main() {
-	int t;
-	cin>>t;
-	while(t--){
-		 int n,k;  
-        cin>>n>>k;
-        if(n==k){
-        	cout<<1<<"\n";
+string mulString(string s1,string s2)
+{
+	int len1=s1.size();
+	int len2=s2.size();
+	vector<int> result(len1+len2,0);
+	for(int i=len1-1;i>=0;i--)
+	{
+		for(int j=len2-1;j>=0;j--)
+		{
+			int product=(s1[i]-'0')*(s2[j]-'0');
+			int sum=product+result[i+j+1];
+			result[i+j+1]=sum%10;
+			result[i+j]+=sum/10;
 		}
-		else{
-        int c=n-k+1;
-    string product = calculateProduct(c,n);
-    cout<< product <<"\n";
-}
 	}
-    return 0;
+	string ans;
+	for(int x:result)
+	{
+		if(!(ans.empty()&&x==0))
+		{
+			ans+=to_string(x);
+		}
+	}
+	return ans.empty()?"0":ans;
 }
+string result(int a,int b)
+{
+	string product="1";
+	for(int i=a;i<=b;i++)
+	{
+		string mul=to_string(i);
+		product=mulString(product,mul);
+	}
+	return product;
+}
+int main()
+{
+	int t; cin >> t;
+	while(t--)
+	{
+		int n,k;
+		cin >> n >> k;
+		int c=n-k+1;
+		cout << result(c,n) << endl;
+	}
+	return 0;
+}
+	
