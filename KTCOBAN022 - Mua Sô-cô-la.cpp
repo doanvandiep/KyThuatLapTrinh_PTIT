a76fcf2e1bed8e47851f5dfcea3dcd652bd8d83c
Đoàn Văn Điệp <147433897+doanvandiep@users.noreply.github.com>
2023-11-12 09:21:20 +0700

    Add files via upload

---
+++ KTCOBAN022 - Mua Sô-cô-la.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t; cin >> t;
+	while(t--)
+	{
+		int X,Y,M;
+		cin >> X >> Y >> M;
+		int socola=M/X+(M/X-1)/(Y-1);
+		cout << socola << endl;
+	}
+	return 0;
+}
