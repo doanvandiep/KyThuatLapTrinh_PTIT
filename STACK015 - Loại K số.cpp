8d64f6d1b883fc8b5fbded4999fb6badbdaf2099
Đoàn Văn Điệp <147433897+doanvandiep@users.noreply.github.com>
2023-11-24 20:39:43 +0700

    Add files via upload

---
+++ STACK015 - Loại K số.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+ string removeKdigits(string num, int k) {
+        
+        if(num.length() <= k)   
+            return "0";
+        
+    
+        if(k == 0)
+            return num;
+        
+        string res = "";
+        stack <char> s; 
+        
+        s.push(num[0]); 
+        
+        for(int i = 1; i<num.length(); ++i)
+        {
+            while(k > 0 && !s.empty() && num[i] < s.top())
+            {
+                
+                --k;
+                s.pop();
+            }
+            
+            s.push(num[i]);
+            
+           
+            if(s.size() == 1 && num[i] == '0')
+                s.pop();
+        }
+        
+        while(k && !s.empty())
+        {
+        
+            --k;
+            s.pop();
+        }
+        
+        while(!s.empty())
+        {
+            res.push_back(s.top()); 
+            s.pop(); 
+        }
+        
+        reverse(res.begin(),res.end()); 
+        
+        if(res.length() == 0)
+            return "0";
+        
+        return res;
+        
+        
+    }
+int main(){
+  int t;
+  cin>>t;
+  cin.ignore();
+  while(t--){
+    string s;
+    int k;
+    cin>>s>>k;
+    cout<<removeKdigits(s,k)<<"\n";
+  }
+ // system("pause");
+}
